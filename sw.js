d1c46df5eaedf0846fbd7afb36cefae1bc7e7a91
GuNunesB <gununes280@gmail.com>
2025-02-13 19:31:13 -0300

    Add. ServiseWorker

---
+++ sw.js
@@ -0,0 +1,42 @@
+/**
+ * Service worker do mini game
+ * @author Gustavo Nunes Bispo
+ */
+
+// Instalação (cache "armazenamento local")
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('static')
+            .then((cache) => {
+                cache.add('./jokenpo/')
+                cache.add('./jokenpo/index.html')
+                cache.add('./jokenpo/style.css')
+                cache.add('./jokenpo/app.js')
+                cache.add('./jokenpo/img/papel.png')
+                cache.add('./jokenpo/img/pedra.png')
+                cache.add('./jokenpo/img/tesoura.png')
+                cache.add('./jokenpo/img/pc.png')
+                cache.add('./jokenpo/img/pcpapel.png')
+                cache.add('./jokenpo/img/pcpedra.png')
+                cache.add('./jokenpo/img/pctesoura.png')
+            })
+    )
+})
+// Ativação
+self.addEventListener('activate', (event) => {
+    console.log("Ativando o service worker...", event)
+    return self.clients.claim()
+})
+// Interceptação (solicitações https servindo em cache quando off-line)
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response
+                } else {
+                    return fetch(event.request)
+                }
+            })
+    )
+})
